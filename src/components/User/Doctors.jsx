13bad1ef7d27a6e2d171fe7a69d5dda1d8261142
Harsh <harshvchawla998@gmail.com>
2025-04-12 17:54:30 +0530

    colour changes

--- src/components/User/Doctors.jsx
+++ src/components/User/Doctors.jsx
@@ -37,7 +37,8 @@ export default function Doctors() {
                   alt={doctor.name}
                   className="w-full h-80 md:h-96 object-cover"
                 />
-
+                
+                
                 <div className="bg-[#DEE2CF] text-main py-6 px-6">
                   <h4 className="text-lg font-semibold">{doctor.name}</h4>
                   <p className="text-xl font-bold uppercase">
