12749d9b1fb473c94c820c8dbac1ca02d767c52d
HarsHvardhn <harshvchawla997@gmail.com>
2026-01-07 21:24:03 +0530

    c

--- server.js
+++ server.js
@@ -291,7 +291,7 @@ const generateSEOHTML = async (path, dynamicData = null) => {
     <div id="root"></div>
     
     <!-- React App JavaScript -->
-    <script type="module" crossorigin src="/assets/index--3kweMup.js"></script>
+    <script type="module" crossorigin src="/assets/index-Bk_9oCup.js"></script>
     
     <noscript>
         <p>Ta strona wymaga JavaScript do pełnej funkcjonalności.</p>
