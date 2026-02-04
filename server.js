680faa1bad6ea6452fe34954010427adbf8ca6c4
=harsh <harshvchawla997@gmail.com>
2026-02-04 12:46:57 +0530

    changes

--- server.js
+++ server.js
@@ -291,7 +291,7 @@ const generateSEOHTML = async (path, dynamicData = null) => {
     <div id="root"></div>
     
     <!-- React App JavaScript -->
-    <script type="module" crossorigin src="/assets/index-z6gazo3-.js"></script>
+    <script type="module" crossorigin src="/assets/index-CRE9bJTl.js"></script>
     
     <noscript>
         <p>Ta strona wymaga JavaScript do pełnej funkcjonalności.</p>
