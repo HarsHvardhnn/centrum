0b9bdf3f3657df6161f726530b4ddf212da9b0ae
Harsh <harshvchawla997@gmail.com>
2026-03-23 11:25:45 +0530

    Changes

--- server.js
+++ server.js
@@ -278,7 +278,7 @@ const generateSEOHTML = async (path, dynamicData = null) => {
     </script>
     
     <!-- React App CSS and JS will be injected here -->
-    <link rel="stylesheet" crossorigin href="/assets/index-DIsNkV2O.css">
+    <link rel="stylesheet" crossorigin href="/assets/index-MC9B_EC1.css">
 </head>
 <body>
     <!-- SEO Content for crawlers -->
@@ -291,7 +291,7 @@ const generateSEOHTML = async (path, dynamicData = null) => {
     <div id="root"></div>
     
     <!-- React App JavaScript -->
-    <script type="module" crossorigin src="/assets/index-9AYeeTN-.js"></script>
+    <script type="module" crossorigin src="/assets/index-IvYiT6F_.js"></script>
     
     <noscript>
         <p>Ta strona wymaga JavaScript do pełnej funkcjonalności.</p>
