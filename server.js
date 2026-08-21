8115bfbc5d0bf9341ac6026274191683bc62ea65
Harsh <harshvchawla997@gmail.com>
2026-08-21 15:52:09 +0530

    env fix

--- server.js
+++ server.js
@@ -291,7 +291,7 @@ const generateSEOHTML = async (path, dynamicData = null) => {
     <div id="root"></div>
     
     <!-- React App JavaScript -->
-    <script type="module" crossorigin src="/assets/index-BEWGX0po.js"></script>
+    <script type="module" crossorigin src="/assets/index-CxSSdlmH.js"></script>
     
     <noscript>
         <p>Ta strona wymaga JavaScript do pełnej funkcjonalności.</p>
