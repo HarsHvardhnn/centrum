90e3530bb847be21f9ed46142c32210e36bea565
Harsh <harshvchawla997@gmail.com>
2026-09-12 09:40:08 +0530

    pushing build

--- server.js
+++ server.js
@@ -724,7 +724,7 @@ const generateSEOHTML = async (path, dynamicData = null) => {
     <div id="root"></div>
     
     <!-- React App JavaScript -->
-    <script type="module" crossorigin src="/assets/index-Bqnid6Ut.js"></script>
+    <script type="module" crossorigin src="/assets/index-BN2UgfIj.js"></script>
     
     <noscript>
         <p>Ta strona wymaga JavaScript do pełnej funkcjonalności.</p>
